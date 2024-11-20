5c6d87f50b5297a891dee0605153dd9f18a23e7b
eddieluis37 <eddierada@gmail.com>
2024-11-19 22:51:42 -0500

    Primera parte del nuevo modulo producto son livewire

--- resources/views/layouts/theme/sidebar.blade.php
+++ resources/views/layouts/theme/sidebar.blade.php
@@ -1066,6 +1066,11 @@
         <div class=" submenu" id="products">
             <ul class="submenu-list" data-parent-element="#products">
                 <li>
+                    <a href="{{ url('producto') }}"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle">
+                                <circle cx="12" cy="12" r="10"></circle>
+                            </svg></span> Productos </a>
+                </li>
+                <li>
                     <a href="{{ url('meatcuts') }}"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle">
                                 <circle cx="12" cy="12" r="10"></circle>
                             </svg></span> Productos Basicos </a>
