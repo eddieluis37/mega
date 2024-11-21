23b6b9a76880f3359a5f466dd4d89053af4e9b48
eddieluis37 <eddieluis37@users.noreply.github.com>
2024-11-21 16:39:28 -0500

    Actualiza centro de costos en el seeder

--- database/seeders/CentrocostoSeeder.php
+++ database/seeders/CentrocostoSeeder.php
@@ -17,12 +17,57 @@ class CentrocostoSeeder extends Seeder
     public function run()
     {
         Centrocosto::create([
-        'name' => 'GUADALUPE',
-        'prefijo' => 'GUA',
+            'name' => 'PLANTA',
+            'prefijo' => 'PLA',
         ]);
         Centrocosto::create([
-        'name' => 'MINUTO DE DIOS',
-        'prefijo' => 'MIN',
-        ]);       
+            'name' => 'GASTOS Y MANTENIMIENTO VEHICULOS',
+            'prefijo' => 'GV',
+        ]);
+        Centrocosto::create([
+            'name' => 'GASTOS Y MANTENIMIENTO MAQUINARIA',
+            'prefijo' => 'GM',
+        ]);
+        Centrocosto::create([
+            'name' => 'PLANTA2',
+            'prefijo' => '',
+        ]);
+        Centrocosto::create([
+            'name' => 'CARNES FRIAS MEGA',
+            'prefijo' => 'CFM',
+        ]);
+        Centrocosto::create([
+            'name' => 'MEGACENTRAL',
+            'prefijo' => 'MC',
+        ]);
+        Centrocosto::create([
+            'name' => 'GUADALUPE',
+            'prefijo' => 'GUA',
+        ]);
+        Centrocosto::create([
+            'name' => 'GALAN',
+            'prefijo' => 'GA',
+        ]);
+        Centrocosto::create([
+            'name' => 'PALOQUEMAO',
+            'prefijo' => 'PA',
+        ]);
+
+        Centrocosto::create([
+            'name' => 'SUBAZAR',
+            'prefijo' => '',
+        ]);
+        Centrocosto::create([
+            'name' => 'CERDO CENTRAL',
+            'prefijo' => 'CC',
+        ]);
+        Centrocosto::create([
+            'name' => 'LECHONERIA',
+            'prefijo' => 'LE',
+        ]);
+        Centrocosto::create([
+            'name' => 'GRANJA',
+            'prefijo' => 'GR',
+        ]);
     }
 }
